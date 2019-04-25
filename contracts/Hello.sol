22abb453a778171bd1863fa925c426990090d127
José Dahlquist <jose.dahlquist@gmail.com>
2019-04-25 11:07:20 -0300

    adds permission check

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.5.0;
 contract Hello {
     // storage property, as it is private can't be accessed from the outside
     string private greet;
+    address private owner;
 
     // defaults greet
     constructor() public {
         greet = "hello world";
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "forbidden action");
+        _;
     }
 
     // public function to get the current value of greet (as it is view doesn't affect storage)
@@ -15,7 +22,7 @@ contract Hello {
     }
 
     // public function to set a new value for greet (this DOES change storage!)
-    function setGreeting(string memory greet_) public {
+    function setGreeting(string memory greet_) public onlyOwner {
         greet = greet_;
     }
 }
